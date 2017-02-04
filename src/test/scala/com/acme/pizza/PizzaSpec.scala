6f2bc5ddc7a1b9bb047c87f898c5b72fde2f036e
Jade Dickinson <jade@Jades-MBP.home>
2017-02-04 17:55:31 +0000

    Move new tests to top of spec

--- src/test/scala/com/acme/pizza/PizzaSpec.scala
+++ src/test/scala/com/acme/pizza/PizzaSpec.scala
@@ -10,23 +10,6 @@ class PizzaSpec extends FunSpec with GivenWhenThen {
 
   describe("A Pizza") {
 
-    it ("should allow the addition of toppings") {
-      Given("a new pizza")
-      pizza = new Pizza
-
-      When("a topping is added")
-      pizza.addTopping(Topping("green olives"))
-
-      Then("the topping count should be incremented")
-      expectResult(1) {
-        pizza.getToppings.size
-      }
-
-      And("the topping should be what was added")
-      val t = pizza.getToppings(0)
-      assert(t === new Topping("green olives"))
-    }
-
     it("Should start with no toppings") {
       Given("a new pizza")
       pizza = new Pizza
@@ -47,6 +30,24 @@ class PizzaSpec extends FunSpec with GivenWhenThen {
         pizza.getToppings.size
       }
     }
+    
+    it ("should allow the addition of toppings") {
+      Given("a new pizza")
+      pizza = new Pizza
+
+      When("a topping is added")
+      pizza.addTopping(Topping("green olives"))
+
+      Then("the topping count should be incremented")
+      expectResult(1) {
+        pizza.getToppings.size
+      }
+
+      And("the topping should be what was added")
+      val t = pizza.getToppings(0)
+      assert(t === new Topping("green olives"))
+    }
+
   }
 
 }
